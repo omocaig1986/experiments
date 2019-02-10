6de92331ace70b3d850c7c773ba40cbd494819e9
Gabriele Proietti Mattia <gabry.gabry@hotmail.it>
2019-02-10 19:39:39 +0100

    Updated port for test

--- multi-get-tester/pigo-face-detect/start_test.sh
+++ multi-get-tester/pigo-face-detect/start_test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 python multi_get.py -m "0.6" \
                     -k 8 \
-                    -u "http://192.168.99.100:8080/function/pigo-face-detector" \
+                    -u "http://192.168.99.100:18080/function/pigo-face-detector" \
                     -p "/home/gabrielepmattia/Coding/p2p-fog/experiments/multi-get-tester/pigo-face-detect/blobs/family.jpg" \
                     --start-ro "0.65" \
                     --end-ro "1.50"
