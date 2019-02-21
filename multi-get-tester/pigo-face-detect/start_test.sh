347300cb79d1d9bc0517d4be4bc53720ac8058aa
Gabriele Proietti Mattia <gabry.gabry@hotmail.it>
2019-02-21 16:36:42 +0100

    Added new math model simulation and sage notebook

--- multi-get-tester/pigo-face-detect/start_test.sh
+++ multi-get-tester/pigo-face-detect/start_test.sh
@@ -3,5 +3,5 @@ python multi_get.py -k 20 \
                     -u "http://192.168.99.100:18080/function/pigo-face-detector" \
                     -p "/Users/gabry3795/Coding/p2p-fog/experiments/multi-get-tester/pigo-face-detect/blobs/family.jpg" \
                     --start-lambda "1" \
-                    --end-lambda "30" \
+                    --end-lambda "40" \
                     --lambda-delta "1.0"
