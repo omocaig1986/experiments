84dc69302a0dd5e69cd802e6578389c56080306f
Gabriele Proietti Mattia <gabry.gabry@hotmail.it>
2019-02-27 10:22:19 +0100

    Added pigo deps

--- benchmark/start_test.sh
+++ benchmark/start_test.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 python bench_multi_get.py -k 10 \
                     -u "http://192.168.99.100:18080/function/exponential-loop" \
-                    --start-lambda "0.1" \
-                    --end-lambda "10" \
+                    --start-lambda "1" \
+                    --end-lambda "5" \
                     --lambda-delta "0.1" \
+                    --requests "200" \
                     -r
 
 
-#  -p "/Users/gabry3795/Coding/p2p-fog/experiments/multi-get-tester/pigo-face-detect/blobs/family.jpg" \
+#  -p "/Users/gabry3795/Coding/p2p-fog/experiments/benchmark/blobs/family.jpg" \
