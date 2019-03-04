3a7f9ce7f8081dd59418fa35ec585ff342f256fe
Gabriele Proietti Mattia <gabry.gabry@hotmail.it>
2019-03-04 10:58:37 +0100

    Set test to lambda 10

--- benchmark/start_multi_machine_test.sh
+++ benchmark/start_multi_machine_test.sh
@@ -2,5 +2,5 @@
 python bench_multi_machine.py \
         -f /home/gabrielepmattia/Coding/p2p-fog/experiments/benchmark/bench_multi_machine_lines.txt \
         --start-lambda "1.0" \
-        --end-lambda "1.1" \
+        --end-lambda "10.0" \
         --lambda-delta "0.1"
