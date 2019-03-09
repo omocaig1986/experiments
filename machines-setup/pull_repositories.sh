a180b272d92888cad96646b2952fe7e4f720b209
Gabriele Proietti Mattia <gabry.gabry@hotmail.it>
2019-03-09 09:31:37 +0100

    Updated pull script

--- machines-setup/pull_repositories.sh
+++ machines-setup/pull_repositories.sh
@@ -1,12 +1,16 @@
 cd ..
 echo "Pulling..."
 git lfs pull
+git pull
 cd ../stack
 echo "Pulling stack..."
 git lfs pull
+git pull
 echo "Pulling scheduler..."
 cd ../stack-scheduler
 git lfs pull
+git pull
 echo "Pulling discovery..."
 cd ../stack-discovery
 git lfs pull
+git pull
