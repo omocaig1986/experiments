cf18bf75174cead69951f5594cf33fc7e74fba89
Gabriele Proietti Mattia <gabry.gabry@hotmail.it>
2019-03-12 20:49:45 +0100

    Added write to file for sync script

--- machines-setup/python_scripts/sync_machines.py
+++ machines-setup/python_scripts/sync_machines.py
@@ -1,5 +1,6 @@
 import subprocess
-import warnings
+import os
+from time import localtime, strftime
 
 SSH_USERNAME = "docker"
 
@@ -9,19 +10,31 @@ commands = [
     "\"cd /home/docker/code/p2p-fog/experiments/machines-setup ; bash -c ./pigo_deploy.sh\"",
 ]
 
+time_str = strftime("%m%d%Y-%H%M%S", localtime())
+dir_path = "./_sync-" + time_str
+os.makedirs(dir_path, exist_ok=True)
+
 hosts_file = open("hosts.txt", "r")
 for host in hosts_file:
     hosts.append(host.strip())
 hosts_file.close()
 
-print("> got %d hosts" % len(hosts))
+print("> got %d hosts\n" % len(hosts))
 
 i = 0
 for host in hosts:
     i += 1
+    j = 0
     for cmd in commands:
+        j += 1
         command = "ssh {0}@{1} {2}".format(SSH_USERNAME, host, cmd)
         print("> [%2d/%2d] Executing %s" % (i, len(hosts), command))
         (status, output) = subprocess.getstatusoutput(command)
+        # print the output to file
+        outfile = open(
+            "{0}/machine-{1}-command-{2}.txt".format(dir_path, i, j), "w")
+        outfile.write(output)
+        outfile.close()
+
         print("> [%2d/%2d] Done! Result %s" % (i, len(hosts), status))
     print()
