9270a5b8e06f21b63a6c46f6e793053a91e33eb4
Gabriele Proietti Mattia <gabry.gabry@hotmail.it>
2019-03-12 17:51:21 +0100

    Added ssh sync machines and deploy pigo

---
+++ machines-setup/pigo_deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cd ../functions/pigo-openfaas
+faas-cli login -u admin --password admin
+faas-cli build
+faas-cli deploy
---
+++ machines-setup/python_scripts/sync_machines.py
@@ -0,0 +1,25 @@
+import subprocess
+import warnings
+
+SSH_USERNAME = "centos"
+
+hosts = []
+commands = [
+    "cd /home/docker/code/p2p-fog/experiments/machines-setup && ./pull_repositories.sh && ./deploy_stack.sh",
+    "cd /home/docker/code/p2p-fog/experiments/machines-setup && ./pigo_deploy.sh",
+]
+
+hosts_file = open("hosts.txt", "r")
+for host in hosts_file:
+    hosts.append(host.strip())
+hosts_file.close()
+
+print("> got %d hosts" % len(hosts))
+
+for host in hosts:
+    for cmd in commands:
+        command = "ssh {0}@{1} {2}".format(SSH_USERNAME, host, cmd)
+        output = subprocess.getoutput(command)
+        print("> Executed %s" % command)
+        print(output)
+        print()
