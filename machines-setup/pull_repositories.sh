d8e199b4cc6fbf4ddfe15f6c9c693582f367902d
Gabriele Proietti Mattia <gabry.gabry@hotmail.it>
2019-03-12 21:28:54 +0100

    Fixed cd in pull reps script

--- machines-setup/pull_repositories.sh
+++ machines-setup/pull_repositories.sh
@@ -2,15 +2,15 @@ cd ..
 echo "Pulling..."
 git lfs pull
 git pull
-cd ../../stack
+cd ../stack
 echo "Pulling stack..."
 git lfs pull
 git pull
 echo "Pulling scheduler..."
-cd ../../stack-scheduler
+cd ../stack-scheduler
 git lfs pull
 git pull
 echo "Pulling discovery..."
-cd ../../stack-discovery
+cd ../stack-discovery
 git lfs pull
 git pull
