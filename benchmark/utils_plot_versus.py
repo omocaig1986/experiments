d58bb9163c3063d907f229632248ecffafc52bdf
Gabriele Proietti Mattia <gabry.gabry@hotmail.it>
2019-03-24 09:37:52 +0100

    Compare plots with model

--- benchmark/utils_plot_versus.py
+++ benchmark/utils_plot_versus.py
@@ -31,26 +31,42 @@ def parseLogFile(file_path):
     return d
 
 
-def start_plot(first_file, second_file, first_title, second_title, x_axis, y_axis, out_dir):
+def start_plot(first_file, second_file, first_title, second_title, x_axis, y_axis, out_dir, model_name, model_k, model_job_len):
     file_1 = parseLogFile(first_file)
     file_2 = parseLogFile(second_file)
 
-    def plotData(feature, dict_1, dict_2):
+    def plotData(feature, dict_1, dict_2, dict_model=None):
         print("{}-{}-vs-{}".format(feature, first_title, second_title))
         plt.clf()
-        line_1, = plt.plot(dict_1[DICT_LAMBDA], dict_1[feature])
-        line_2, = plt.plot(dict_2[DICT_LAMBDA], dict_2[feature])
-        plt.legend([line_1, line_2], [first_title, second_title])
+        line_1, = plt.plot(dict_1[DICT_LAMBDA], dict_1[feature], c="C0")
+        line_2, = plt.plot(dict_2[DICT_LAMBDA], dict_2[feature], c="C2")
+
+        if dict_model != None:
+            line_model, = plt.plot(dict_1[DICT_LAMBDA], dict_model, c="C1")
+            plt.legend([line_1, line_2, line_model], [first_title, second_title, model_name])
+        else:
+            plt.legend([line_1, line_2], [first_title, second_title])
 
         # plt.title("{0} - LL({1}, K-{2}) - (K={3},μ={4:.4f}) - Machine#{5}".format(function, f, t, k, mi, i))
         plt.xlabel(x_axis)
         plt.ylabel(feature)
-        plt.savefig("{}/{}-{}-vs-{}.pdf".format(out_dir, feature, first_title, second_title))
+
+        if dict_model != None:
+            plt.savefig("{}/{}-{}-vs-{}-with-model.pdf".format(out_dir, feature, first_title, second_title))
+        else:
+            plt.savefig("{}/{}-{}-vs-{}.pdf".format(out_dir, feature, first_title, second_title))
 
     os.makedirs(out_dir, exist_ok=True)
-    plotData(DICT_DELAY, file_1, file_2)
-    plotData(DICT_PB, file_1, file_2)
-    plotData(DICT_PE, file_1, file_2)
+
+    if model_name == "":
+        plotData(DICT_DELAY, file_1, file_2)
+        plotData(DICT_PB, file_1, file_2)
+        plotData(DICT_PE, file_1, file_2)
+    else:
+        plotData(DICT_DELAY, file_1, file_2, model_mm1k.generateDelayArray(
+            file_1[DICT_LAMBDA], model_k, 1.0 / model_job_len))
+        plotData(DICT_PB, file_1, file_2, model_mm1k.generatePbArray(file_1[DICT_LAMBDA], model_k, 1.0 / model_job_len))
+        plotData(DICT_PE, file_1, file_2)
 
 
 def main(argv):
@@ -62,10 +78,14 @@ def main(argv):
     y_axis = ""
     out_dir = ""
 
+    model_name = ""
+    model_k = 10
+    model_job_len = 1
+
     usage = "utils_plot_versus.py"
     try:
         opts, args = getopt.getopt(
-            argv, "h", ["first-file=", "second-file=", "first-title=", "second-title=", "x-axis=", "y-axis=", "out-dir="])
+            argv, "h", ["first-file=", "second-file=", "first-title=", "second-title=", "x-axis=", "y-axis=", "out-dir=", "model-name=", "model-k=", "model-job-len="])
     except getopt.GetoptError:
         print(usage)
         sys.exit(2)
@@ -88,6 +108,12 @@ def main(argv):
             y_axis = arg
         elif opt in ("--out-dir"):
             out_dir = arg
+        elif opt in ("--model-name"):
+            model_name = arg
+        elif opt in ("--model-k"):
+            model_k = int(arg)
+        elif opt in ("--model-job-len"):
+            model_job_len = float(arg)
 
     if first_file == "":
         print("Some needed parameter was not given")
@@ -102,10 +128,15 @@ def main(argv):
     print("> x_axis %s" % x_axis)
     print("> y_axis %s" % y_axis)
     print("----")
+    print("> model_name %s" % model_name)
+    print("> model_k %s" % model_k)
+    print("> model_job_len %s" % model_job_len)
+    print("----")
     print("> out_dir %s" % out_dir)
     print("")
 
-    start_plot(first_file, second_file, first_title, second_title, x_axis, y_axis, out_dir)
+    start_plot(first_file, second_file, first_title, second_title, x_axis,
+               y_axis, out_dir, model_name, model_k, model_job_len)
 
 
 if __name__ == "__main__":
--- benchmark/utils_plot_versus.sh
+++ benchmark/utils_plot_versus.sh
@@ -2,9 +2,12 @@ CURRENT_PATH=$(dirname $0)
 
 source $CURRENT_PATH/env/bin/activate
 $CURRENT_PATH/env/bin/python utils_plot_versus.py \
-                                --first-file "/Users/gabry3795/Cloud/Google Drive/Università/Magistrale/Thesis/Data/BladeServers/LL(1,K-2)-16machines-2/results-machine-00.txt" \
+                                --first-file "/Users/gabry3795/Cloud/Google Drive/Università/Magistrale/Thesis/Data/BladeServers/LL(4,K-10)-5machines/results-machine-00.txt" \
                                 --second-file "/Users/gabry3795/Cloud/Google Drive/Università/Magistrale/Thesis/Data/BladeServers/LL(15,K-10)-16machines/results-machine-00.txt" \
-                                --first-title "LL(1,K-2)-16machines-2" \
+                                --first-title "LL(4,K-10)-5machines" \
                                 --second-title "LL(15,K-10)-16machines" \
                                 --x-axis "λ" \
+                                --model-name "M/M/1/10" \
+                                --model-k "10" \
+                                --model-job-len "0.27" \
                                 --out-dir ${CURRENT_PATH}"/tmp"
