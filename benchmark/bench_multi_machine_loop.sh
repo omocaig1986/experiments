7bcd1cf707e8ed77c69ca83e4d7ecb1601147c3b
Gabriele Proietti Mattia <gabry.gabry@hotmail.it>
2019-03-29 09:59:58 +0100

    Changed the range of test for loop

--- benchmark/bench_multi_machine_loop.sh
+++ benchmark/bench_multi_machine_loop.sh
@@ -6,7 +6,7 @@ $CURRENT_PATH/env/bin/python bench_multi_machine.py \
         --hosts-file "$CURRENT_PATH/bench_multi_machine_lines.txt" \
         --function-url "function/exponential-loop" \
         --requests "200" \
-        --start-lambda "1.0" \
-        --end-lambda "10.0" \
+        --start-lambda "0.1" \
+        --end-lambda "3.0" \
         --lambda-delta "0.1" \
         --poisson
