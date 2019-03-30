cec2aca6b501d35745d19865ce9ea9d782e86197
Gabriele Proietti Mattia <gabry.gabry@hotmail.it>
2019-03-30 20:13:56 +0100

    Implemented distribution plot with base data

--- benchmark/utils_plot_times_distribution.py
+++ benchmark/utils_plot_times_distribution.py
@@ -31,21 +31,37 @@ def parseAllFiles(path, prefix, start_lambda, end_lambda, lambda_delta, k, machi
     return d, all_values
 
 
-def start_plot(path, prefix, start_lambda, end_lambda, lambda_delta, k, machine_id, bins):
+def parseFilesList(data_files, prefix, start_lambda, end_lambda, lambda_delta, k, machine_id):
+    all_values = []
+    for path in data_files:
+        d, values = parseAllFiles(path, prefix, start_lambda, end_lambda, lambda_delta, k, machine_id)
+        all_values += values
+    return all_values
+
+
+def start_plot(function, path, prefix, start_lambda, end_lambda, lambda_delta, f, t, k, mi, machine_id, bins, data_files):
     out_plots_dir = "{0}/{1}".format(path, "_plots_distribution")
+    plot_title = "{} - LL({}, K-{}) - (K={},μ={:.4f}) - Machine#{}".format(function,
+                                                                           f, t, k, mi, machine_id)
     os.makedirs(out_plots_dir, exist_ok=True)
     d, all_values = parseAllFiles(path, prefix, start_lambda, end_lambda, lambda_delta, k, machine_id)
-    min_v = min(all_values)
-    max_v = max(all_values)
+    if len(data_files) == 0:
+        min_v = min(all_values)
+        max_v = max(all_values)
+    else:
+        base_values = parseFilesList(data_files, prefix, start_lambda, end_lambda, lambda_delta, k, machine_id)
+        min_v = min(base_values)
+        max_v = max(base_values)
 
     def plotAllValuesHist():
         filename = "all-values-hist-machine{:02}.pdf".format(machine_id)
         print("Plotting %s" % filename)
         plt.clf()
-        plt.hist(all_values, bins=bins)
+        plt.hist(all_values, bins=bins, range=(min_v, max_v))
         plt.xlabel("Delay (s)")
         plt.ylabel("Occurrences")
-        plt.savefig("{}/{}".format(out_plots_dir, filename))
+        plt.title(plot_title)
+        plt.savefig("{}/{}{}".format(out_plots_dir, "" if len(data_files) == 0 else "merged-", filename))
 
     def plotHeatMap():
         filename = "heatmap-hist-machine{:02}.pdf".format(machine_id)
@@ -61,7 +77,7 @@ def start_plot(path, prefix, start_lambda, end_lambda, lambda_delta, k, machine_
         while True:
             x_ticks.append(str(l))
             histogram_data, bins_edges = np.histogram(d[str(l)], bins=bins, range=(min_v, max_v))
-            heat_matrix.append(histogram_data)
+            heat_matrix.append(histogram_data.tolist()[::-1])
             l = round(lambda_delta + l, 2)
             if l > end_lambda:
                 break
@@ -69,10 +85,11 @@ def start_plot(path, prefix, start_lambda, end_lambda, lambda_delta, k, machine_
         for i in range(len(bins_edges)):
             if i + 1 >= len(bins_edges):
                 break
-            y_ticks.append("[{}, {}]".format(str(round(bins_edges[i], 2)), str(round(bins_edges[i+1], 2))))
+            y_ticks.append("[{}, {}]".format(str(round(bins_edges[i], 2)), str(round(bins_edges[i + 1], 2))))
+        y_ticks = y_ticks[::-1]
 
         fig, ax = plt.subplots()
-        im = ax.imshow(np.array(heat_matrix))
+        im = ax.imshow(np.array(matrixSym(heat_matrix)))
         # colorbar
         cbar = ax.figure.colorbar(im, ax=ax)
         cbar.ax.set_ylabel("Occurrences", rotation=-90, va="bottom")
@@ -83,15 +100,23 @@ def start_plot(path, prefix, start_lambda, end_lambda, lambda_delta, k, machine_
         ax.set_yticklabels(y_ticks, fontsize="x-small")
         ax.set_xlabel('λ')
         ax.set_ylabel('Delay (s)')
-        plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
-                 rotation_mode="anchor")
+        plt.setp(ax.get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor")
         # save
+        plt.title(plot_title)
         fig.tight_layout()
-        plt.savefig("{}/{}".format(out_plots_dir, filename))
+        plt.savefig("{}/{}{}".format(out_plots_dir, "" if len(data_files) == 0 else "merged-", filename))
 
     plotAllValuesHist()
     plotHeatMap()
 
+
+def matrixSym(m):
+    out = [None] * len(m)
+    for i in range(len(m)):
+        out[i] = [0.0] * len(m[i])
+        for j in range(len(m[i])):
+            out[i][j] = m[j][i]
+    return out
 #
 # Entrypoint
 #
@@ -111,11 +136,12 @@ def main(argv):
     k = 10
     machine_id = 0
     bins = 10
+    data_files = []
 
     usage = "utils_plot_times_distribution.py"
     try:
         opts, args = getopt.getopt(
-            argv, "hk:p:", ["files-prefix=", "start-lambda=", "end-lambda=", "lambda-delta=", "path=", "function=", "fanout=", "threshold=", "job-duration=", "machine-id=", "bins="])
+            argv, "hk:p:", ["files-prefix=", "start-lambda=", "end-lambda=", "lambda-delta=", "path=", "function=", "fanout=", "threshold=", "job-duration=", "machine-id=", "bins=", "data-files="])
     except getopt.GetoptError as e:
         print(e)
         print(usage)
@@ -149,6 +175,8 @@ def main(argv):
             machine_id = int(arg)
         elif opt in ("--bins"):
             bins = int(arg)
+        elif opt in ("--data-files"):
+            data_files = arg.split(":")
 
     if files_path == "":
         print("Some needed parameter was not given")
@@ -167,10 +195,12 @@ def main(argv):
     print("> k %d" % k)
     print("> machine_id %d" % machine_id)
     print("> bins %d" % bins)
+    print("> data_files %s" % data_files)
     print("")
 
     mi = 1.0 / job_duration
-    start_plot(files_path, files_prefix, start_lambda, end_lambda, lambda_delta, k, machine_id, bins)
+    start_plot(function, files_path, files_prefix, start_lambda, end_lambda,
+               lambda_delta, fanout, threshold, k, mi, machine_id, bins, data_files)
 
 
 if __name__ == "__main__":
--- benchmark/utils_plot_times_distribution.sh
+++ benchmark/utils_plot_times_distribution.sh
@@ -13,4 +13,5 @@ $CURRENT_PATH/env/bin/python utils_plot_times_distribution.py --files-prefix "re
                         --end-lambda "3.0" \
                         --lambda-delta "0.1" \
                         --bins "30" \
-                        --machine-id "0"
+                        --machine-id "0" \
+                        --data-files "/Users/gabry3795/Cloud/Google Drive/Università/Magistrale/Thesis/Data/BladeServers/MultiNode/Loop/LL(1,K-2)-16machines-heat/_test_multi_machine-03302019-163137:/Users/gabry3795/Cloud/Google Drive/Università/Magistrale/Thesis/Data/BladeServers/SingleNode/Loop/NS(K10)-heat/_test_multi_get-03302019-114919"
