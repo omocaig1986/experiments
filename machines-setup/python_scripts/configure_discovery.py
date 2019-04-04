7e9c0420e19867e3128bd2a6b5e0e89960074ee1
Gabriele Proietti Mattia <gabry.gabry@hotmail.it>
2019-04-04 17:49:40 +0200

    Discovery configurator skips lines with #

--- machines-setup/python_scripts/configure_discovery.py
+++ machines-setup/python_scripts/configure_discovery.py
@@ -19,13 +19,15 @@ def preparePayload(host_id, host_ip, init_servers):
 
 def setConfiguration(host_id, host_ip, init_servers):
     # prepare request
-    url = "http://{0}:{1}/{2}".format(host_ip, SERVICE_PORT, API_CONFIGURATION_URL)
+    url = "http://{0}:{1}/{2}".format(host_ip,
+                                      SERVICE_PORT, API_CONFIGURATION_URL)
     headers = {'Content-Type': "application/json"}
     ok = True
 
     print("\r[%s] %s configuring..." % (status_str.CHECK_STR, host_ip), end="")
     try:
-        res = requests.post(url, data=preparePayload(host_id, host_ip, init_servers), headers=headers, timeout=5)
+        res = requests.post(url, data=preparePayload(
+            host_id, host_ip, init_servers), headers=headers, timeout=5)
     except (requests.Timeout, requests.ConnectionError):
         print("\r[%s] %s is not responding" % (status_str.DEAD_STR, host_ip))
         ok = False
@@ -35,12 +37,15 @@ def setConfiguration(host_id, host_ip, init_servers):
         if res.status_code != 200:
             print_str = status_str.DEAD_STR
 
-        print("\r[%s] %s set as \"%s\" [%s]" % (print_str, host_ip, host_id, res.status_code))
+        print("\r[%s] %s set as \"%s\" [%s]" %
+              (print_str, host_ip, host_id, res.status_code))
 
 
 conf_file = open("configure_discovery.txt", "r")
 
 for line in conf_file:
+    if line == "#":
+        continue
     comp = line.split()
     host_ip = comp[1]
     host_id = comp[0]
