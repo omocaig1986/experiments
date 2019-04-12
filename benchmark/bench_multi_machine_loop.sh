71799707a754840aaa3c82c1f3129331578e9a86
Gabriele Proietti Mattia <gabry.gabry@hotmail.it>
2019-04-12 19:00:28 +0200

    Changed hosts list file in benchmark dir

--- benchmark/bench_multi_machine_loop.sh
+++ benchmark/bench_multi_machine_loop.sh
@@ -3,7 +3,7 @@ CURRENT_PATH=$(dirname $0)
 
 source $CURRENT_PATH/env/bin/activate
 $CURRENT_PATH/env/bin/python bench_multi_machine.py \
-        --hosts-file "$CURRENT_PATH/bench_multi_machine_lines.txt" \
+        --hosts-file "$CURRENT_PATH/hosts.txt" \
         --function-url "function/exponential-loop" \
         --requests "200" \
         --start-lambda "0.1" \
--- benchmark/bench_multi_machine_pigo.sh
+++ benchmark/bench_multi_machine_pigo.sh
@@ -3,7 +3,7 @@ CURRENT_PATH=$(dirname $0)
 
 source $CURRENT_PATH/env/bin/activate
 $CURRENT_PATH/env/bin/python bench_multi_machine.py \
-        --hosts-file "$CURRENT_PATH/bench_multi_machine_lines.txt" \
+        --hosts-file "$CURRENT_PATH/hosts.txt" \
         --function-url "function/pigo-face-detector" \
         --payload "$CURRENT_PATH/blobs/family.jpg" \
         --requests "200" \
