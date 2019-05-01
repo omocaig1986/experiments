eb989fb94cfe62a0c6a10f4f587643a40fe60ff1
Gabriele Proietti Mattia <gabry.gabry@hotmail.it>
2019-05-01 14:48:03 +0200

    Added registry address to deploy stack

--- machines-setup/deploy_stack.sh
+++ machines-setup/deploy_stack.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # login
-docker login -u gitlab+deploy-token-64025 -p CfNK37-zH4oV1xDstDe6
-docker login -u gitlab+deploy-token-64030 -p zNCW2p4iJG6CpPsxnnqx
+docker registry.gitlab.com login -u "gitlab+deploy-token-64025" -p "CfNK37-zH4oV1xDstDe6"
+docker registry.gitlab.com login -u "gitlab+deploy-token-64030" -p "zNCW2p4iJG6CpPsxnnqx"
 
 cd ../../
 cd stack
