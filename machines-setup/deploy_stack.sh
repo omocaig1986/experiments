35e302cebdebb0d79c754b92c711881d3621df21
Gabriele Proietti Mattia <gabry.gabry@hotmail.it>
2019-05-01 14:46:25 +0200

    Fixed deploy stack script

--- machines-setup/deploy_stack.sh
+++ machines-setup/deploy_stack.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
-# deploy the stack
+# login
 docker login -u gitlab+deploy-token-64025 -p CfNK37-zH4oV1xDstDe6
 docker login -u gitlab+deploy-token-64030 -p zNCW2p4iJG6CpPsxnnqx
+
+cd ../../
+cd stack
 
 docker stack rm p2p-fog
 docker stack deploy -c docker-compose.yml p2p-fog
