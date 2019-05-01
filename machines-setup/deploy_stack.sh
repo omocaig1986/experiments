64020e0cae71cb26bd21939d7e2b5468ceb87e13
Gabriele Proietti Mattia <gabry.gabry@hotmail.it>
2019-05-01 14:45:45 +0200

    Added deploy token even for scheduler

--- machines-setup/deploy_stack.sh
+++ machines-setup/deploy_stack.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # deploy the stack
-docker login -u "gitlab+deploy-token-64025" -p "CfNK37-zH4oV1xDstDe6"
+docker login -u gitlab+deploy-token-64025 -p CfNK37-zH4oV1xDstDe6
+docker login -u gitlab+deploy-token-64030 -p zNCW2p4iJG6CpPsxnnqx
 
 docker stack rm p2p-fog
 docker stack deploy -c docker-compose.yml p2p-fog
