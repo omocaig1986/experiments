b11b27f66d2defcfdf8485ea1fb8914feda5a67e
Gabriele Proietti Mattia <gabry.gabry@hotmail.it>
2019-05-01 15:13:26 +0200

    Updated sync routing

--- machines-setup/python_scripts/sync_machines.py
+++ machines-setup/python_scripts/sync_machines.py
@@ -16,7 +16,7 @@ commands = [
     "\"cd /home/docker/code/p2p-fog/experiments/machines-setup ; bash -c ./undeploy_pigo.sh\"",
     "\"cd /home/docker/code/p2p-fog/experiments/machines-setup ; bash -c ./undeploy_stack.sh\"",
     "\"cd /home/docker/code/p2p-fog/experiments/machines-setup ; bash -c ./update_faas.sh\"",
-    "\"cd /home/docker/code/p2p-fog/experiments/machines-setup ; bash -c ./pull_repositories.sh && ./deploy_stack.sh\"",
+    "\"cd /home/docker/code/p2p-fog/experiments/machines-setup ; bash -c ./pull_repositories.sh && ./deploy_stack_local.sh\"",
     "\"cd /home/docker/code/p2p-fog/experiments/machines-setup ; bash -c ./deploy_pigo.sh\"",
     "docker system prune -f --volumes",
     "sudo reboot"
