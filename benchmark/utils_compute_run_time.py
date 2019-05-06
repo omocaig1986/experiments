6635cc8583f298bfc0de48e2496015503c2a1b1f
Gabriele Proietti Mattia <gabry.gabry@hotmail.it>
2019-05-06 18:58:22 +0200

    Updated compute time script

--- benchmark/utils_compute_run_time.py
+++ benchmark/utils_compute_run_time.py
@@ -1,7 +1,8 @@
-start_lambda = 3.50
-end_lambda = 3.50
+start_lambda = 1.0
+end_lambda = 3.60
 lambda_delta = 0.05
-requests = 25000
+requests = 200
+thresholds = 10
 
 l = start_lambda
 total_time = 0.0
@@ -12,6 +13,7 @@ while True:
     l = round(l + lambda_delta, 2)
     if l > end_lambda:
         break
+total_time *= thresholds
 
 print("===== Stimate run time =====")
 print("> start_lambda %.2f" % start_lambda)
@@ -20,8 +22,13 @@ print("> lambda_delta %.2f" % lambda_delta)
 print("> requests %d" % requests)
 print()
 
-hrs = int(total_time / 3600)
-mins = int((total_time - hrs * 3600) / 60)
-secs = total_time - hrs*3600 - mins*60
+time = total_time
+day = time // (24 * 3600)
+time = time % (24 * 3600)
+hour = time // 3600
+time %= 3600
+minutes = time // 60
+time %= 60
+seconds = time
 
-print("Total apprx. time %.2fs = %dh%dm%ds" % (total_time, hrs, mins, secs))
+print("Total apprx. time %.2fs = %dd%dh%dm%ds" % (total_time, day, hour, minutes, seconds))
