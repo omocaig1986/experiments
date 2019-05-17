3ce4a95e4b1c68529a57ff977067c8fd14c138d2
Gabriele Proietti Mattia <gabry.gabry@hotmail.it>
2019-05-17 14:10:26 +0200

    Added arm detection in deploy stack local

--- machines-setup/deploy_stack_local.sh
+++ machines-setup/deploy_stack_local.sh
@@ -9,7 +9,14 @@ docker build -t scheduler:latest .
 cd ..
 cd stack
 docker stack rm p2p-fog
-docker stack deploy -c docker-compose-local.yml p2p-fog
+
+if (( $(uname -m | cut -b 1-3) == "arm" ));
+then
+    echo "==> ARM architecture detected"
+    docker stack deploy -c docker-compose-local.armhf.yml p2p-fog
+else 
+    docker stack deploy -c docker-compose-local.yml p2p-fog
+fi
 
 # remove unused images and containers
 docker system prune -f --volumes
