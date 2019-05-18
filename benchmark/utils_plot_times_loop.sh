70ffef977f08f573c9d683f3fe7518cb09a0e9c8
Gabriele Proietti Mattia <gabry.gabry@hotmail.it>
2019-05-18 16:22:07 +0200

    Updated loop plot

--- benchmark/utils_plot_times_loop.sh
+++ benchmark/utils_plot_times_loop.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 CURRENT_PATH=$(dirname $0)
-T=0
+
+f=1
 
 #!/bin/bash
-for i in {0..9}
+for i in {0..10}
 do
 
     source $CURRENT_PATH/env/bin/activate
     $CURRENT_PATH/env/bin/python utils_plot_times.py --files-prefix "results-machine-" \
                             --files-n "8" \
-                            --path "/Users/gabry3795/Coding/p2p-fog/experiments-data/BladeServers/debian/PigoFaceDetect/MultiNode/25000reqs-2/LL(1,K-$i)-8machines" \
+                            --path "/Users/gabry3795/Coding/p2p-fog/experiments-data/BladeServers/debian/PigoFaceDetect/LL(1,K)/700reqs/LL($f,K-$i)-8machines" \
                             --function "Pigo Face Detect" \
-                            --fanout "1" \
+                            --fanout $f \
                             --threshold $i \
-                            --job-duration "0.27" \
+                            --job-duration "0.301769" \
                             --with-model \
                             --model-name "M/M/1/10" \
                             -k "10"
