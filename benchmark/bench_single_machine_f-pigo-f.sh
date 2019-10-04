0a3dd332c3b356adf3400423e786fbffc32f619c
Gabriele Proietti Mattia <gabry.gabry@hotmail.it>
2019-10-04 20:29:36 +0200

    Added single machine pigo-f, fixed typo

---
+++ benchmark/bench_single_machine_f-pigo-f.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+CURRENT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+# shellcheck disable=SC1090
+source "$CURRENT_PATH"/env/bin/activate
+"$CURRENT_PATH"/env/bin/python bench_single_machine.py \
+  --host "192.168.99.100:18080" \
+  --function-url "function/pigo-face-detector-f" \
+  --start-lambda "1" \
+  --end-lambda "10" \
+  --lambda-delta "0.1" \
+  --requests "200" \
+  --poisson
+
+#  -p "/Users/gabry3795/Coding/p2p-fog/experiments/benchmark/blobs/family.jpg" \
--- benchmark/bench_single_machine_series.sh
+++ benchmark/bench_single_machine_series.sh
@@ -3,7 +3,7 @@ CURRENT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 # shellcheck disable=SC1090
 source "$CURRENT_PATH"/env/bin/activate
-"$CURRENT_PATH"/env/bin/python becn_single_machine_series.py \
+"$CURRENT_PATH"/env/bin/python bench_single_machine_series.py \
   --host "192.168.99.100:18080" \
   --function "function/pigo-face-detector" \
   --payload "$CURRENT_PATH/blobs/family.jpg" \
