6ef0ebcc29f2e867c644eccfb1a7e82cfc7ebdf4
Gabriele Proietti Mattia <gabry.gabry@hotmail.it>
2019-11-26 11:15:15 +0100

    Fixed undeploy_pigo_f script

--- machines-setup/undeploy_pigo_f.sh
+++ machines-setup/undeploy_pigo_f.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd ../functions/pigo-openfaas-f
 faas-cli login -u admin --password admin
-faas-cli remove pigo-face-detector
+faas-cli remove pigo-face-detector-f
 docker system prune -f --volumes
