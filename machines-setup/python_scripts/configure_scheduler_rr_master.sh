3f1285690877b9d2b5e753640c070281ef531050
Gabriele Proietti Mattia <gabry.gabry@hotmail.it>
2019-12-01 10:09:21 +0100

    Updated IPs numbers for benchmarking

--- machines-setup/python_scripts/configure_scheduler_rr_master.sh
+++ machines-setup/python_scripts/configure_scheduler_rr_master.sh
@@ -5,4 +5,4 @@ CURRENT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 source "$CURRENT_PATH"/env/bin/activate
 "$CURRENT_PATH"/env/bin/python "$CURRENT_PATH"/configure_scheduler.py \
   --host "192.168.1.173" \
-  --scheduler "RoundRobinWithMasterScheduler true 192.168.1.173 true"
+  --scheduler "RoundRobinWithMasterScheduler true 192.168.1.251 true"
