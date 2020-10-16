a3d9ac59978021429a5215d5be3a5d7cdae84cb3
Gabriele Proietti Mattia <pm.gabriele@outlook.com>
2020-10-16 11:29:57 +0200

    Updated deploy stack local

--- machines-setup/deploy_stack_local.sh
+++ machines-setup/deploy_stack_local.sh
@@ -10,14 +10,16 @@ cd stack-scheduler
 docker build -t scheduler:latest .
 cd ..
 cd stack
-docker stack rm p2p-fog
+
+docker stack rm p2p-faas
+docker stack rm p2p-fog # temporary
 
 if [ $(uname -m | cut -b 1-3) == "arm" ]
 then
     echo "==> ARM architecture detected"
-    docker stack deploy -c docker-compose-local.armhf.yml p2p-fog
+    docker stack deploy -c docker-compose-local.armhf.yml p2p-faas
 else 
-    docker stack deploy -c docker-compose-local.yml p2p-fog
+    docker stack deploy -c docker-compose-local.yml p2p-faas
 fi
 
 # remove unused images and containers
