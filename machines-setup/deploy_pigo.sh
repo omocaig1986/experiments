ab62d68de62badadefc936797b3dec4bd750c23a
Gabriele Proietti Mattia <pm.gabriele@outlook.com>
2020-10-16 11:57:53 +0200

    Updated faas-cli dir

--- machines-setup/deploy_pigo.sh
+++ machines-setup/deploy_pigo.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+FAAS_CLI=~/bin/faas-cli
+
 cd ../functions/pigo-openfaas
-faas-cli login -u admin --password admin
-faas-cli build
-faas-cli deploy
+$FAAS_CLI login -u admin --password admin
+$FAAS_CLI build
+$FAAS_CLI deploy
 docker system prune -f --volumes
