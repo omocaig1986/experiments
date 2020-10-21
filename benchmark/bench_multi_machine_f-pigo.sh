f39db696e764823b553065ad1adb54921894ed0d
Gabriele Proietti Mattia <pm.gabriele@outlook.com>
2020-10-21 13:41:00 +0200

    Added bench script for pwr_n_tau

--- benchmark/bench_multi_machine_f-pigo.sh
+++ benchmark/bench_multi_machine_f-pigo.sh
@@ -7,8 +7,8 @@ source "$CURRENT_PATH"/env/bin/activate
   --hosts-file "$CURRENT_PATH/hosts.txt" \
   --function-url "function/pigo-face-detector" \
   --payload "$CURRENT_PATH/blobs/family.jpg" \
-  --requests "200" \
-  --start-lambda "0.1" \
-  --end-lambda "3.8" \
+  --requests "1000" \
+  --start-lambda "5.50" \
+  --end-lambda "5.50" \
   --lambda-delta "0.05" \
   --poisson
---
+++ benchmark/bench_multi_machine_multi_tau_f-pigo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Bench multiple values of the threshold, from $1 to $2
+#
+CURRENT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+echo "=== Starting loop from tau=$1 to tau=$2, delta_tau=$3, T=$4 ==="
+
+for tau in { $1..$2..$3 }; do
+  echo "=> Setting tau=$tau"
+  "$CURRENT_PATH"/../machines-setup/python_scripts/configure_pwr_n_tau_scheduler.sh $4 "${tau}ms"
+  sleep 30
+  echo "=> Starting test with tau=$i"
+  "$CURRENT_PATH"/bench_multi_machine_f-pigo.sh
+  sleep 120
+done
