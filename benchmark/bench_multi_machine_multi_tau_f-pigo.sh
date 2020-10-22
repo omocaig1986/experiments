0fa71b48c36ed6814463eb29fdb2f5036017930a
Gabriele Proietti Mattia <pm.gabriele@outlook.com>
2020-10-22 14:32:06 +0200

    Updated bench multi tau script

--- benchmark/bench_multi_machine_multi_tau_f-pigo.sh
+++ benchmark/bench_multi_machine_multi_tau_f-pigo.sh
@@ -6,11 +6,11 @@ CURRENT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 echo "=== Starting loop from tau=$1 to tau=$2, delta_tau=$3, T=$4 ==="
 
-for tau in { $1..$2..$3 }; do
+for ((tau = $1; tau <= $2; tau = tau + $3)); do
   echo "=> Setting tau=$tau"
   "$CURRENT_PATH"/../machines-setup/python_scripts/configure_pwr_n_tau_scheduler.sh "$4" "${tau}ms"
-  sleep 30
+  sleep 15
   echo "=> Starting test with tau=$tau"
   "$CURRENT_PATH"/bench_multi_machine_f-pigo.sh
-  sleep 120
+  sleep 60
 done
