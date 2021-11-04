f0f78e628e5f1c2ef92822c3f8b48965d79fe4da
omocaig1986 <gia.magnani@gmail.com>
2021-11-04 23:02:35 +0100

    Added start_p2p_local_faas.sh

---
+++ machines-setup/start_p2p_local_faas.sh
@@ -0,0 +1,20 @@
+./delete_machines.sh
+./create_machines.sh
+
+./setup_machines.sh
+./setup_hosts.txt.sh
+
+cd python_scripts
+
+git add .
+git commit -m "Updated files hosts.txt and configure_discovery.txt"
+git push origin master
+
+./clone_stack.sh
+
+./sync_machines.sh
+
+
+./configure_discovery.sh
+
+./configure_no_scheduler.sh
