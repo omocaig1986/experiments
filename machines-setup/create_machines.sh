91884494c408ba99f465a4c85a3e9875da9684ef
omocaig1986 <gia.magnani@gmail.com>
2021-11-04 20:53:35 +0100

    updated create_machines.sh

--- machines-setup/create_machines.sh
+++ machines-setup/create_machines.sh
@@ -2,10 +2,3 @@ docker-machine create -d virtualbox --virtualbox-no-vtx-check  n1
 docker-machine create -d virtualbox --virtualbox-no-vtx-check  n2
 docker-machine create -d virtualbox --virtualbox-no-vtx-check  n3
 docker-machine create -d virtualbox --virtualbox-no-vtx-check  n4
-
-ssh-copy-id docker@$(docker-machine ip n1)
-ssh-copy-id docker@$(docker-machine ip n2)
-ssh-copy-id docker@$(docker-machine ip n3)
-ssh-copy-id docker@$(docker-machine ip n4)
-
-ssh -o StrictHostKeyChecking=no -o ConnectTimeout=5 docker@$(docker-machine ip n1) "tce-load -wi coreutils -y"
