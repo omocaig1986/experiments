5da1202bcc365c2b1942a14edaad46eac875d9e2
omocaig1986 <gia.magnani@gmail.com>
2021-11-04 21:15:43 +0100

    updated setup_hosts_txt.sh

--- machines-setup/setup_hosts_txt.sh
+++ machines-setup/setup_hosts_txt.sh
@@ -5,3 +5,12 @@ rm ./python_scripts/hosts.txt
     echo $(docker-machine ip n3)
     echo $(docker-machine ip n4)
 } > ./python_scripts/hosts.txt
+
+rm ./python_scripts/configure_discovery.txt
+
+{
+    echo docker@$n1 $(docker-machine ip n1)
+    echo docker@$n2 $(docker-machine ip n2)
+    echo docker@$n3 $(docker-machine ip n3)
+    echo docker@$n4 $(docker-machine ip n4)
+} > ./python_scripts/configure_discovery.txt
