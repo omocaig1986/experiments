5b152acebd525fcbf3ca75472290fb1606a40528
omocaig1986 <gia.magnani@gmail.com>
2021-11-04 22:50:09 +0100

    updated configuration files txt

--- machines-setup/python_scripts/configure_discovery.sh
+++ machines-setup/python_scripts/configure_discovery.sh
@@ -3,4 +3,4 @@ CURRENT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 # shellcheck disable=SC1090
 source "$CURRENT_PATH"/env/Scripts/activate
-"$CURRENT_PATH"/env/Scripts/python configure_discovery.py $1
+"$CURRENT_PATH"/env/Scripts/python configure_discovery.py ./configure_discovery.txt
