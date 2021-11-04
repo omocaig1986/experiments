c14b4ddb1cd079e84e83ea3d6d5856337f07257e
omocaig1986 <gia.magnani@gmail.com>
2021-11-04 23:11:45 +0100

    Updated files hosts.txt and configure_discovery.txt

--- machines-setup/python_scripts/sync_machines.sh
+++ machines-setup/python_scripts/sync_machines.sh
@@ -3,4 +3,4 @@ CURRENT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 # shellcheck disable=SC1090
 source "$CURRENT_PATH"/env/Scripts/activate
-"$CURRENT_PATH"/env/Scripts/python sync_machines.py $1 $2
+"$CURRENT_PATH"/env/Scripts/python sync_machines.py docker ./hosts.txt
